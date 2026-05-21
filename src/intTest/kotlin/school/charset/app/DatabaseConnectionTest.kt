38c05ff96d3038e2c7d28a373925680dac6bfa7e
Florent Sorel <florent.sorel@gmail.com>
2026-05-21 17:50:00 +0200

    fix linter

--- src/intTest/kotlin/school/charset/app/DatabaseConnectionTest.kt
+++ src/intTest/kotlin/school/charset/app/DatabaseConnectionTest.kt
@@ -5,11 +5,9 @@ import io.kotest.matchers.shouldBe
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.postgresql.PostgreSQLContainer
