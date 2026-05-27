725d1af684f8d53b9fd4b7d1619ac2477f1a6eed
Florent Sorel <florent.sorel@gmail.com>
2026-05-27 16:31:23 +0200

    fix ktlint

--- src/main/kotlin/school/charset/app/infrastructure/http/sandbox/SandboxController.kt
+++ src/main/kotlin/school/charset/app/infrastructure/http/sandbox/SandboxController.kt
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import school.charset.app.domain.encoding.CodePoint
 import school.charset.app.domain.encoding.Codec
 import school.charset.app.domain.encoding.Encoding
 import school.charset.app.domain.sandbox.SandboxBytesParser
